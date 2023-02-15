5331b62359d434d8ca14515873c30fdbbd504e45
Pierre Constantineau <jpconstantineau@gmail.com>
2023-02-15 15:31:47 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BlueMicro_Examples_Arduino_Library
+
+Keyboard examples that uses the set of BlueMicro Arduino Libraries.
---
+++ src/bluemicro_examples.h
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2022 BlueMicro_Examples_Arduino_Library  contributors (https://github.com/jpconstantineau/BlueMicro_Examples_Arduino_Library/graphs/contributors)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef BLUEMICRO_EXAMPLES_H
+#define BLUEMICRO_EXAMPLES_H
+
+#endif
